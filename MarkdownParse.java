d7fdeccdd64706e506491d86cd4c1774de58db83
YueSteveYin <98061226+YueSteveYin@users.noreply.github.com>
2022-01-28 01:03:44 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -7,16 +7,14 @@ import java.util.ArrayList;
 public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
-        // find the next [, then find the ], then find the (, then take up to
-        // the next )
-        int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
-            int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            int openParen = markdown.indexOf("(", nextCloseBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+        String[] input = markdown.split("\n", 0);
+        for(int i = 0; i<input.length; i++)
+        {
+            if(input[i].contains("[") && input[i].contains("]") && (input[i].contains("link")|| input[i].contains("Link")))
+            {
+                String website = input[i].substring(input[i].indexOf("(")+1, input[i].lastIndexOf(")"));
+                toReturn.add(website);
+            }
         }
         return toReturn;
     }
